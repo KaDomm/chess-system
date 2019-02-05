87bf05dbb10235183fb1cd19c1fa5d19f73cd44c
KaDomm <kkadomm@gmail.com>
2019-02-05 08:20:03 +0000

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -28,14 +29,18 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	/***********************************************************************************/
+
 	public static void clearScreen() {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
 	}
 
-	public static ChessPosition readChessPossition(Scanner sc) {
-		try {
+	/***********************************************************************************/
 
+	public static ChessPosition readChessPossition(Scanner sc) {
+
+		try {
 			String s = sc.nextLine();
 			char column = s.charAt(0);
 			int row = Integer.parseInt(s.substring(1));
@@ -45,6 +50,17 @@ public class UI {
 			throw new InputMismatchException("Errer reading ChessPosition. Valid values are from a1 to h8");
 		}
 	}
+
+	/***********************************************************************************/
+
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println("\nTurn: " + chessMatch.getTurn());
+		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		
+	}
+	
+	/***********************************************************************************/
 
 	public static void printBoard(ChessPiece[][] pieces) {
 
@@ -59,7 +75,8 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 	}
 	
-	
+	/***********************************************************************************/
+
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 
 		for (int i = 0; i < pieces.length; i++) {
@@ -73,6 +90,7 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 	}
 
+	/***********************************************************************************/
 
 	public static void printPiece(ChessPiece piece, boolean background) {
 		
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,86 +6,126 @@ import boardgame.Position;
 import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knight;
+import chess.pieces.Pawn;
 import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
 
+	private Integer turn;
+	private Color currentPlayer;
 	private Board board;
 
+	/***********************************************************************************/
+
 	public ChessMatch() {
+
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
 	}
 
-	
+	/***********************************************************************************/
+
+	public Integer getTurn() {
+		return turn;
+	}
+
+	/***********************************************************************************/
+
+	public Color getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	/***********************************************************************************/
+
 	public ChessPiece[][] getPieces() {
 
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+
 		for (int i = 0; i < board.getRows(); i++)
-			for (int j = 0; j < board.getColumns(); j++) {
+			for (int j = 0; j < board.getColumns(); j++)
 				mat[i][j] = (ChessPiece) board.piece(i, j);
-			}
 
 		return mat;
-
 	}
-	
-	
-	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+
+	/***********************************************************************************/
+
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
 		Position position = sourcePosition.toPosition();
 		validateSourcePosition(position);
 		return board.piece(position).possibleMoves();
 	}
-	
-	
+
+	/***********************************************************************************/
+
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
-		
+
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
-		
+
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 
 		Piece captuerPiece = makeMove(source, target);
-		
+
+		nextTurn();
+
 		return (ChessPiece) captuerPiece;
 	}
-	
-	
+
+	/***********************************************************************************/
+
 	private Piece makeMove(Position source, Position target) {
-		
+
 		Piece p = board.removePiece(source);
-		
+
 		Piece capturedPiece = board.removePiece(target);
-		
+
 		board.placePiece(p, target);
-		
+
 		return capturedPiece;
 	}
 
-	
+	/***********************************************************************************/
+
 	private void validateSourcePosition(Position position) {
-		
-		if(!board.thereIsAPiece(position))
+
+		if (!board.thereIsAPiece(position))
 			throw new ChessException("There is no piece on source position");
-		
-		if(!board.piece(position).isThereAnyPossibleMove()) 
+
+		if (currentPlayer != ((ChessPiece) board.piece(position)).getColor())
+			throw new ChessException("The chosen piece is not yours");
+
+		if (!board.piece(position).isThereAnyPossibleMove())
 			throw new ChessException("There is no possible moves for the chosen piece");
-	}
-	
-	private void validateTargetPosition(Position source, Position target) {
-		if(!board.piece(source).possibleMove(target))
-			throw new ChessException("The chosen pience can't move to target position");
-		
+
 	}
 
-	
+	/***********************************************************************************/
+
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target))
+			throw new ChessException("The chosen pience can't move to target position");
+	}
+
+	/***********************************************************************************/
+
+	private void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+	}
+
+	/***********************************************************************************/
+
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
 
-	
+	/***********************************************************************************/
+
 	private void initialSetup() {
 		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
@@ -97,7 +137,7 @@ public class ChessMatch {
 		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
 		/*for (int i = 0; i < 8; i++)
 			placeNewPiece((char) ('a' + i), 7, new Pawn(board, Color.BLACK));*/
-		
+
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
@@ -108,7 +148,6 @@ public class ChessMatch {
 		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
 		/*for (int i = 0; i < 8; i++)
 			placeNewPiece((char) ('a' + i), 2, new Pawn(board, Color.WHITE));*/
-
 	}
 
 }
